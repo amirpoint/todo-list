25e9c39452a8138068e1e2f0dd202f41e04b8d3f
amirpoint <onceler51@gmail.com>
2022-10-18 17:22:00 +0330

    Add get item from client and redirect to list

--- app.js
+++ app.js
@@ -2,18 +2,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 const app = express();
 const port = 3000;
+let items = ["First item", "Second item", "3nd item"];//default items
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true})); 
 
 
 app.get("/", function (req, res) {
     let today = new Date();
     let day = today.toLocaleDateString("en-US", {weekday: "long", day: "numeric", month: "long"});
 
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: day, newListItems: items});//ejs method
+
+});
+
+
+app.post("/", function (req, res) {
+    //get and push new item to the array list
+    items.push(req.body.newItem);
+    console.log(items[items.length-1]);
+    res.redirect("/");
 
 });
 
