05740e690036bc0245213a14e44c1f6b8bbd8024
amirpoint <onceler51@gmail.com>
2022-10-19 13:45:30 +0330

    Add /work page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 let items = ["First item", "Second item", "3nd item"];//default items
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true})); 
@@ -14,16 +15,28 @@ app.get("/", function (req, res) {
     let today = new Date();
     let day = today.toLocaleDateString("en-US", {weekday: "long", day: "numeric", month: "long"});
 
-    res.render("list", {kindOfDay: day, newListItems: items});//ejs method
+    res.render("list", {listTitle: day, newListItems: items});//ejs method
+
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 
 });
 
 
 app.post("/", function (req, res) {
-    //get and push new item to the array list
-    items.push(req.body.newItem);
-    console.log(items[items.length-1]);
-    res.redirect("/");
+    //get and push new item to the specific array list
+    if (req.body.btn === "Work List") {
+        workItems.push(req.body.newItem);
+        res.redirect("/work");
+
+    } else {
+        items.push(req.body.newItem);
+        res.redirect("/");     
+        
+    }
+    
 
 });
 
